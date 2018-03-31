a761f1474184dc25ef3cee4c6e10bc4ba4bc9909
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2018-03-31 14:48:22 -0400

    Update start script

--- scripts/start-client.js
+++
@@ -1,4 +0,0 @@
-// From https://medium.com/of-all-things-tech-progress/starting-with-authentication-a-tutorial-with-node-js-and-mongodb-25d524ca0359
-const args = [ 'start' ];
-const opts = { stdio: 'inherit', cwd: 'client', shell: true };
-require('child_process').spawn('npm', args, opts);
