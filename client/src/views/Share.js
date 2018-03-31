676e18ed68baaa7c62f8ae1d59f051aa6bdf2d40
JuJuLy <july.zhu@mail.utoronto.ca>
2018-03-31 16:50:29 -0400

    updating share and save selected gif

--- client/src/views/Share.js
+++ client/src/views/Share.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import twitter from '../images/sharing/twitter.svg';
 import facebook from '../images/sharing/facebook.svg';
 import giphy from '../images/sharing/giphy.png';
+import saveCoffee from '../images/logo.svg';
 import '../style/Share.css'
+
+//session_start();
 
 class Share extends Component {
   constructor(props) {
@@ -12,12 +15,14 @@ class Share extends Component {
       gif: ""
     }
     this.createQuery = this.createQuery.bind(this);
+    this.save= this.save.bind(this);
   }
   componentDidMount(){
     window.scrollTo(0, 0);
     this.createQuery();
   }
   render(){
+
     return (
       <div className="shareBox">
         <img src={this.state.gif} alt="Gif could not be shown" className="gif"/>
@@ -31,6 +36,10 @@ class Share extends Component {
         <a href={this.state.gif} target="_blank">
           <img src={giphy} alt="Share button not found" className="shareButton"/>
         </a>
+        <a onClick={this.save}>
+          <img src={saveCoffee} alt="Save button not found" className="shareButton" />
+
+        </a>
       </div>
     );
   }
@@ -40,5 +49,44 @@ class Share extends Component {
     image = this.props.location.search.substring(5); // Removes the ?gif=
     this.setState({gif: image});
   }
+
+  save(e) {
+
+    e.preventDefault();
+   
+    if (localStorage.getItem("isLoggedIn")){
+
+      console.log("type is " + typeof(localStorage.getItem("uid"))+ " and uid is " + localStorage.getItem("uid"));
+      
+      fetch('/api/gifs/'+ localStorage.getItem("uid"), {
+          method: 'PUT',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            gif: this.state.gif
+          })
+      }).then(response => {
+            if (response.ok) {
+                window.alert("Selected gif has been saved.");
+                return JSON.stringify({result:"successfully saved"});
+            }
+            else {
+                console.log("Save Failed");
+                return("Save Error");
+            }
+      })
+
+      //redirect to login page
+    } else {
+      
+      window.alert("Please log in first to save.");
+      window.location.href = "/#/login"; 
+    }
+  }
+
 }
+
 export default Share;
+
